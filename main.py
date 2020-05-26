e3fecbf27b81cfaebecfa86152c18df6e909f0c0
Lucas Brown <lmbrown@myst.ai>
2020-05-25 18:54:20 -0700

    implementing cubic spline in r, refactoring max 14 day run

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import datetime
 
 import gspread
+import numpy as np
 import pandas as pd
 import requests
+import rpy2.robjects as robjects
 import scipy.interpolate as interpolate
 from df2gspread import df2gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -61,7 +63,7 @@ STATE_SUMMARY_COLUMNS = [
 CDC_GUIDANCE_GOOGLE_WORKBOOK_KEY = "1s534JoVjsetLDUxzkww3yQSnRj9H-8QLMKPUrq7RAuc"
 FOR_WEBSITE_TAB_NAME = "For Website"
 ALL_STATE_DATA_TAB_NAME = "All State Data"
-WORK_IN_PROGRESS_CA_ONLY_TAB_NAME = "Work in Progress (California Only)"
+WORK_IN_PROGRESS_CA_ONLY_TAB_NAME = "Work in Progress (NY Only)"
 STATE_SUMMARY_TAB_NAME = "State Summary"
 
 
@@ -173,13 +175,44 @@ def fit_and_predict_cubic_spline(series_):
 
     # Note: you can't simply uses pd.Series.interpolate because that will only fill in data for the `nan` values.
     predicted_spline_values = interpolate.UnivariateSpline(
-        x=substitute_index,
-        y=series_.values,
-        k=3,
-        # For reference, the CDC uses this method with an `spar` of .5, as seen in line 384 of `Trajectory Analysis.R`:
-        # https://www.rdocumentation.org/packages/stats/versions/3.6.2/topics/smooth.spline
-        s=0.5,
+        x=substitute_index, y=series_.values, k=3
     )(substitute_index)
+
+    predicted_spline_series = pd.Series(
+        data=predicted_spline_values, index=series_.index
+    )
+
+    return predicted_spline_series
+
+
+def fit_and_predict_cubic_spline_in_r(
+    series_, smoothing_parameter=None, replace_nan=True
+):
+    if not smoothing_parameter:
+        # Import `NULL` from R.
+        smoothing_parameter = robjects.r["as.null"]()
+
+    # Assert that the index is sorted.
+    if not series_.index.is_monotonic_increasing:
+        raise ValueError("Index is not sorted.")
+
+    # Replace nans
+    if replace_nan:
+        series_ = series_.fillna(value=0)
+
+    r_x = robjects.DateVector(series_.index)
+    r_y = robjects.FloatVector(series_.values.astype(float))
+
+    # Extract R's smoothing function.
+    r_smooth_spline = robjects.r["smooth.spline"]
+
+    # For reference, the CDC uses this method with an `spar` of .5, as seen in line 384 of `Trajectory Analysis.R`:
+    # https://www.rdocumentation.org/packages/stats/versions/3.6.2/topics/smooth.spline
+    fitted_spline = r_smooth_spline(x=r_x, y=r_y, spar=smoothing_parameter)
+
+    predicted_spline_values = list(
+        robjects.r["predict"](fitted_spline, robjects.FloatVector(r_x)).rx2("y")
+    )
 
     predicted_spline_series = pd.Series(
         data=predicted_spline_values, index=series_.index
@@ -223,8 +256,9 @@ def transform_covidtracking_data(df):
         # Calculate the cubic spline on the 3 day average of total cases.
         df.loc[
             (state,), TOTAL_CASES_3_DAY_AVERAGE_CUBIC_SPLINE_FIELD
-        ] = fit_and_predict_cubic_spline(
-            df.loc[(state,), TOTAL_CASES_3_DAY_AVERAGE_FIELD]
+        ] = fit_and_predict_cubic_spline_in_r(
+            series_=df.loc[(state,), TOTAL_CASES_3_DAY_AVERAGE_FIELD],
+            smoothing_parameter=0.5,
         ).values
 
         # Calculate 3-day rolling average of new cases.
@@ -238,8 +272,9 @@ def transform_covidtracking_data(df):
         )
 
         # Calculate the cubic spline on the 3 day average of total cases.
-        df.loc[(state,), NEW_CASES_3DCS_FIELD] = fit_and_predict_cubic_spline(
-            df.loc[(state,), NEW_CASES_3_DAY_AVERAGE_FIELD]
+        df.loc[(state,), NEW_CASES_3DCS_FIELD] = fit_and_predict_cubic_spline_in_r(
+            series_=df.loc[(state,), NEW_CASES_3_DAY_AVERAGE_FIELD],
+            smoothing_parameter=0.5,
         ).values
 
         # Calculate 3DCS new cases diff.
@@ -250,25 +285,25 @@ def transform_covidtracking_data(df):
         # Calculate consecutive increases or decreases.
         df.loc[
             (state,), CONSECUTIVE_INCREASE_NEW_CASES_3DCS_FIELD
-        ] = find_consecutive_positive_or_negative_values(
+        ] = get_consecutive_positive_or_negative_values(
             series_=df.loc[(state,), NEW_CASES_3DCS_DIFF_FIELD], positive_values=True
         ).values
 
         df.loc[
             (state,), CONSECUTIVE_DECREASE_NEW_CASES_3DCS_FIELD
-        ] = find_consecutive_positive_or_negative_values(
+        ] = get_consecutive_positive_or_negative_values(
             series_=df.loc[(state,), NEW_CASES_3DCS_DIFF_FIELD], positive_values=False
         ).values
 
         # Calculate criteria 1A: must see at least 9 days of a decrease in new cases over a 14 day window.
         df.loc[
             (state,), MAX_RUN_OF_DECREASING_NEW_CASES_IN_14_DAY_WINDOW_3DCS_FIELD
-        ] = (
-            df.loc[(state,), CONSECUTIVE_DECREASE_NEW_CASES_3DCS_FIELD]
-            .rolling(window=14, min_periods=1, center=False)
-            .max()
-            .values
-        )
+        ] = get_max_run_in_window(
+            series_=df.loc[(state,), NEW_CASES_3DCS_DIFF_FIELD],
+            positive_values=False,
+            window_size=14,
+        ).values
+
         df.loc[(state,), CDC_CRITERIA_1A_COVID_CONTINUOUS_DECLINE_FIELD] = (
             df.loc[
                 (state,), MAX_RUN_OF_DECREASING_NEW_CASES_IN_14_DAY_WINDOW_3DCS_FIELD
@@ -279,17 +314,17 @@ def transform_covidtracking_data(df):
         # Calculate criteria 1B: must not see 5 or more days of an increase in new cases over a 14 day window.
         df.loc[
             (state,), MAX_RUN_OF_INCREASING_NEW_CASES_IN_14_DAY_WINDOW_3DCS_FIELD
-        ] = (
-            df.loc[(state,), CONSECUTIVE_INCREASE_NEW_CASES_3DCS_FIELD]
-            .rolling(window=14, min_periods=1, center=False)
-            .max()
-            .values
-        )
+        ] = get_max_run_in_window(
+            series_=df.loc[(state,), NEW_CASES_3DCS_DIFF_FIELD],
+            positive_values=True,
+            window_size=14,
+        ).values
+
         df.loc[(state,), CDC_CRITERIA_1B_COVID_NO_REBOUNDS_FIELD] = (
             df.loc[
                 (state,), MAX_RUN_OF_INCREASING_NEW_CASES_IN_14_DAY_WINDOW_3DCS_FIELD
             ]
-            <= 5
+            < 5
         ).values
 
         # Calculate criteria 1C: new cases on T-0 must be < T-14.
@@ -313,7 +348,7 @@ def transform_covidtracking_data(df):
     return df
 
 
-def find_consecutive_positive_or_negative_values(series_, positive_values=True):
+def get_consecutive_positive_or_negative_values(series_, positive_values=True):
     meets_criteria = series_ >= 0 if positive_values else series_ < 0
     consecutive_positive_values = meets_criteria * (
         meets_criteria.groupby(
@@ -322,6 +357,31 @@ def find_consecutive_positive_or_negative_values(series_, positive_values=True):
         + 1
     )
     return consecutive_positive_values
+
+
+def get_max_run_in_window(series_, positive_values, window_size=14):
+    # Assert that the index is sorted.
+    if not series_.index.is_monotonic_increasing:
+        raise ValueError("Index is not sorted.")
+
+    returned_series = pd.Series(index=series_.index, data=np.nan)
+
+    for i in range(window_size - 1, len(series_)):
+        # Start calculating the run of values that happened *within (and only within)* this window.
+        # TODO(lbrown): this is incredibly inefficient, but I can't think of a faster way while following the right
+        #  interpretation of the rules.
+        consecutive_positive_or_negative_values = get_consecutive_positive_or_negative_values(
+            series_=series_.iloc[i + 1 - window_size : i + 1],
+            positive_values=positive_values,
+        )
+
+        # Find the max run.
+        returned_series[i] = consecutive_positive_or_negative_values.max()
+
+        if i == len(series_) - 1:
+            pass
+
+    return returned_series
 
 
 def post_covidtracking_data(df, workbook_key, tab_name, credentials):
@@ -374,7 +434,7 @@ if __name__ == "__main__":
 
     # Upload data for just CA.
     post_covidtracking_data(
-        df=transformed_df.loc[transformed_df[STATE_SOURCE_FIELD] == "CA",],
+        df=transformed_df.loc[transformed_df[STATE_SOURCE_FIELD] == "NY",],
         workbook_key=CDC_GUIDANCE_GOOGLE_WORKBOOK_KEY,
         tab_name=WORK_IN_PROGRESS_CA_ONLY_TAB_NAME,
         credentials=credentials,
--- main_test.py
+++ main_test.py
@@ -5,21 +5,25 @@ import numpy as np
 import pandas as pd
 from pandas.testing import assert_series_equal
 
-from main import find_consecutive_positive_or_negative_values
+from main import (
+    get_consecutive_positive_or_negative_values,
+    fit_and_predict_cubic_spline_in_r,
+    get_max_run_in_window,
+)
 from main import fit_and_predict_cubic_spline
 
 
 class MainTest(unittest.TestCase):
     def test_find_consecutive_positive_values(self):
         assert_series_equal(
-            find_consecutive_positive_or_negative_values(
+            get_consecutive_positive_or_negative_values(
                 series_=pd.Series([-1, 1, 2, -4, 3, 5]), positive_values=True
             ),
             pd.Series([0, 1, 2, 0, 1, 2]),
         )
 
         assert_series_equal(
-            find_consecutive_positive_or_negative_values(
+            get_consecutive_positive_or_negative_values(
                 series_=pd.Series([-1, -1, 0, 1, 2, 3, -15, -17.0]),
                 positive_values=True,
             ),
@@ -28,7 +32,7 @@ class MainTest(unittest.TestCase):
 
         # Make sure we handle null values appropriately.
         assert_series_equal(
-            find_consecutive_positive_or_negative_values(
+            get_consecutive_positive_or_negative_values(
                 series_=pd.Series([np.nan, 1, 2, -4, np.nan, 5]), positive_values=True
             ),
             pd.Series([0, 1, 2, 0, 0, 1]),
@@ -36,7 +40,7 @@ class MainTest(unittest.TestCase):
 
         # Make sure we handle null values appropriately.
         assert_series_equal(
-            find_consecutive_positive_or_negative_values(
+            get_consecutive_positive_or_negative_values(
                 series_=pd.Series([np.nan, np.nan, 2, 4, np.nan, 5]),
                 positive_values=True,
             ),
@@ -45,14 +49,14 @@ class MainTest(unittest.TestCase):
 
         # Now, test for negative values.
         assert_series_equal(
-            find_consecutive_positive_or_negative_values(
+            get_consecutive_positive_or_negative_values(
                 series_=pd.Series([-1, 1, 2, -4, 3, 5]), positive_values=False
             ),
             pd.Series([1, 0, 0, 1, 0, 0]),
         )
 
         assert_series_equal(
-            find_consecutive_positive_or_negative_values(
+            get_consecutive_positive_or_negative_values(
                 series_=pd.Series([-1, -1, -2, 0, -7, 5]), positive_values=False
             ),
             pd.Series([1, 2, 3, 0, 1, 0]),
@@ -115,3 +119,401 @@ class MainTest(unittest.TestCase):
                     ),
                 )
             )
+
+    def test_fit_and_predict_cubic_spline_in_r(self):
+        # Test with default smoothing parameter.
+        assert_series_equal(
+            fit_and_predict_cubic_spline_in_r(
+                pd.Series(
+                    data=[-1, 1, 2, -4, 3, 5],
+                    index=pd.DatetimeIndex(
+                        data=[
+                            pd.to_datetime("2020-01-01"),
+                            pd.to_datetime("2020-01-02"),
+                            pd.to_datetime("2020-01-03"),
+                            pd.to_datetime("2020-01-04"),
+                            pd.to_datetime("2020-01-05"),
+                            pd.to_datetime("2020-01-06"),
+                        ],
+                        freq=datetime.timedelta(days=1),
+                    ),
+                )
+            ),
+            pd.Series(
+                data=[
+                    -1.1428568397306358,
+                    -0.2857142936746852,
+                    0.5714282888471683,
+                    1.4285710563026406,
+                    2.285714184172537,
+                    3.1428575140383694,
+                ],
+                index=[
+                    pd.to_datetime("2020-01-01"),
+                    pd.to_datetime("2020-01-02"),
+                    pd.to_datetime("2020-01-03"),
+                    pd.to_datetime("2020-01-04"),
+                    pd.to_datetime("2020-01-05"),
+                    pd.to_datetime("2020-01-06"),
+                ],
+            ),
+        )
+
+        # Test with null values.
+        assert_series_equal(
+            fit_and_predict_cubic_spline_in_r(
+                pd.Series(
+                    data=[np.nan, np.nan, 2, -4, 3, 5],
+                    index=pd.DatetimeIndex(
+                        data=[
+                            pd.to_datetime("2020-01-01"),
+                            pd.to_datetime("2020-01-02"),
+                            pd.to_datetime("2020-01-03"),
+                            pd.to_datetime("2020-01-04"),
+                            pd.to_datetime("2020-01-05"),
+                            pd.to_datetime("2020-01-06"),
+                        ],
+                        freq=datetime.timedelta(days=1),
+                    ),
+                ),
+                replace_nan=True,
+            ),
+            pd.Series(
+                data=[
+                    -0.6854102994488032,
+                    -0.21069773360008473,
+                    0.33578816306613873,
+                    1.067404321401707,
+                    2.12766929964248,
+                    3.365246248938551,
+                ],
+                index=[
+                    pd.to_datetime("2020-01-01"),
+                    pd.to_datetime("2020-01-02"),
+                    pd.to_datetime("2020-01-03"),
+                    pd.to_datetime("2020-01-04"),
+                    pd.to_datetime("2020-01-05"),
+                    pd.to_datetime("2020-01-06"),
+                ],
+            ),
+        )
+
+        # Test with explicit smoothing parameter.
+        assert_series_equal(
+            fit_and_predict_cubic_spline_in_r(
+                series_=pd.Series(
+                    data=[-1, 1, 2, -4, 3, 5],
+                    index=pd.DatetimeIndex(
+                        data=[
+                            pd.to_datetime("2020-01-01"),
+                            pd.to_datetime("2020-01-02"),
+                            pd.to_datetime("2020-01-03"),
+                            pd.to_datetime("2020-01-04"),
+                            pd.to_datetime("2020-01-05"),
+                            pd.to_datetime("2020-01-06"),
+                        ],
+                        freq=datetime.timedelta(days=1),
+                    ),
+                ),
+                smoothing_parameter=0.5,
+            ),
+            pd.Series(
+                data=[
+                    -0.5415075617582621,
+                    -0.1765863921621331,
+                    0.07061496437277474,
+                    0.5778703926676914,
+                    2.0462976989861517,
+                    4.023310897893784,
+                ],
+                index=[
+                    pd.to_datetime("2020-01-01"),
+                    pd.to_datetime("2020-01-02"),
+                    pd.to_datetime("2020-01-03"),
+                    pd.to_datetime("2020-01-04"),
+                    pd.to_datetime("2020-01-05"),
+                    pd.to_datetime("2020-01-06"),
+                ],
+            ),
+        )
+
+        # Should raise errors due to the index out of order.
+        with self.assertRaises(ValueError):
+            fit_and_predict_cubic_spline_in_r(
+                pd.Series(
+                    data=[-1, 1, 2, -4, 3, 5],
+                    index=pd.DatetimeIndex(
+                        data=[
+                            pd.to_datetime("2020-01-02"),
+                            pd.to_datetime("2020-01-03"),
+                            pd.to_datetime("2020-01-04"),
+                            pd.to_datetime("2020-01-05"),
+                            pd.to_datetime("2020-01-06"),
+                            # This timestamp is out of order.
+                            pd.to_datetime("2020-01-01"),
+                        ],
+                        freq=datetime.timedelta(days=1),
+                    ),
+                )
+            )
+
+    def test_get_max_run_in_window(self):
+        # A very simple series.
+        assert_series_equal(
+            get_max_run_in_window(
+                positive_values=True,
+                window_size=3,
+                series_=pd.Series(
+                    data=[1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    index=pd.DatetimeIndex(
+                        data=[
+                            pd.to_datetime("2020-01-01"),
+                            pd.to_datetime("2020-01-02"),
+                            pd.to_datetime("2020-01-03"),
+                            pd.to_datetime("2020-01-04"),
+                            pd.to_datetime("2020-01-05"),
+                            pd.to_datetime("2020-01-06"),
+                            pd.to_datetime("2020-01-07"),
+                            pd.to_datetime("2020-01-08"),
+                            pd.to_datetime("2020-01-09"),
+                        ],
+                        freq=datetime.timedelta(days=1),
+                    ),
+                ),
+            ),
+            pd.Series(
+                data=[np.nan, np.nan, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0],
+                index=pd.DatetimeIndex(
+                    data=[
+                        pd.to_datetime("2020-01-01"),
+                        pd.to_datetime("2020-01-02"),
+                        pd.to_datetime("2020-01-03"),
+                        pd.to_datetime("2020-01-04"),
+                        pd.to_datetime("2020-01-05"),
+                        pd.to_datetime("2020-01-06"),
+                        pd.to_datetime("2020-01-07"),
+                        pd.to_datetime("2020-01-08"),
+                        pd.to_datetime("2020-01-09"),
+                    ],
+                    freq=datetime.timedelta(days=1),
+                ),
+            ),
+        )
+
+        assert_series_equal(
+            get_max_run_in_window(
+                positive_values=True,
+                window_size=3,
+                series_=pd.Series(
+                    data=[1, 2, 3, -1, 1, 2, -1, -1, -3],
+                    index=pd.DatetimeIndex(
+                        data=[
+                            pd.to_datetime("2020-01-01"),
+                            pd.to_datetime("2020-01-02"),
+                            pd.to_datetime("2020-01-03"),
+                            pd.to_datetime("2020-01-04"),
+                            pd.to_datetime("2020-01-05"),
+                            pd.to_datetime("2020-01-06"),
+                            pd.to_datetime("2020-01-07"),
+                            pd.to_datetime("2020-01-08"),
+                            pd.to_datetime("2020-01-09"),
+                        ],
+                        freq=datetime.timedelta(days=1),
+                    ),
+                ),
+            ),
+            pd.Series(
+                data=[np.nan, np.nan, 3.0, 2.0, 1.0, 2.0, 2.0, 1.0, 0],
+                index=pd.DatetimeIndex(
+                    data=[
+                        pd.to_datetime("2020-01-01"),
+                        pd.to_datetime("2020-01-02"),
+                        pd.to_datetime("2020-01-03"),
+                        pd.to_datetime("2020-01-04"),
+                        pd.to_datetime("2020-01-05"),
+                        pd.to_datetime("2020-01-06"),
+                        pd.to_datetime("2020-01-07"),
+                        pd.to_datetime("2020-01-08"),
+                        pd.to_datetime("2020-01-09"),
+                    ],
+                    freq=datetime.timedelta(days=1),
+                ),
+            ),
+        )
+
+        assert_series_equal(
+            get_max_run_in_window(
+                positive_values=False,
+                window_size=3,
+                series_=pd.Series(
+                    data=[1, 2, 3, -1, 1, 2, -1, -1, -3],
+                    index=pd.DatetimeIndex(
+                        data=[
+                            pd.to_datetime("2020-01-01"),
+                            pd.to_datetime("2020-01-02"),
+                            pd.to_datetime("2020-01-03"),
+                            pd.to_datetime("2020-01-04"),
+                            pd.to_datetime("2020-01-05"),
+                            pd.to_datetime("2020-01-06"),
+                            pd.to_datetime("2020-01-07"),
+                            pd.to_datetime("2020-01-08"),
+                            pd.to_datetime("2020-01-09"),
+                        ],
+                        freq=datetime.timedelta(days=1),
+                    ),
+                ),
+            ),
+            pd.Series(
+                data=[np.nan, np.nan, 0.0, 1.0, 1.0, 1.0, 1.0, 2.0, 3.0],
+                index=pd.DatetimeIndex(
+                    data=[
+                        pd.to_datetime("2020-01-01"),
+                        pd.to_datetime("2020-01-02"),
+                        pd.to_datetime("2020-01-03"),
+                        pd.to_datetime("2020-01-04"),
+                        pd.to_datetime("2020-01-05"),
+                        pd.to_datetime("2020-01-06"),
+                        pd.to_datetime("2020-01-07"),
+                        pd.to_datetime("2020-01-08"),
+                        pd.to_datetime("2020-01-09"),
+                    ],
+                    freq=datetime.timedelta(days=1),
+                ),
+            ),
+        )
+
+        assert_series_equal(
+            get_max_run_in_window(
+                positive_values=True,
+                series_=pd.Series(
+                    data=[
+                        18,
+                        19,
+                        -1,
+                        -2,
+                        -3,
+                        1,
+                        2,
+                        3,
+                        4,
+                        5,
+                        -1,
+                        -19,
+                        -3,
+                        -2,
+                        9,
+                        2,
+                        3,
+                        4,
+                        -1,
+                        -1,
+                        -1,
+                        -1,
+                        -1,
+                        -1,
+                        -1,
+                        -1,
+                        -1,
+                        -1,
+                    ],
+                    index=pd.DatetimeIndex(
+                        data=[
+                            pd.to_datetime("2020-01-01"),
+                            pd.to_datetime("2020-01-02"),
+                            pd.to_datetime("2020-01-03"),
+                            pd.to_datetime("2020-01-04"),
+                            pd.to_datetime("2020-01-05"),
+                            pd.to_datetime("2020-01-06"),
+                            pd.to_datetime("2020-01-07"),
+                            pd.to_datetime("2020-01-08"),
+                            pd.to_datetime("2020-01-09"),
+                            pd.to_datetime("2020-01-10"),
+                            pd.to_datetime("2020-01-11"),
+                            pd.to_datetime("2020-01-12"),
+                            pd.to_datetime("2020-01-13"),
+                            pd.to_datetime("2020-01-14"),
+                            pd.to_datetime("2020-01-15"),
+                            pd.to_datetime("2020-01-16"),
+                            pd.to_datetime("2020-01-17"),
+                            pd.to_datetime("2020-01-18"),
+                            pd.to_datetime("2020-01-19"),
+                            pd.to_datetime("2020-01-20"),
+                            pd.to_datetime("2020-01-21"),
+                            pd.to_datetime("2020-01-22"),
+                            pd.to_datetime("2020-01-23"),
+                            pd.to_datetime("2020-01-24"),
+                            pd.to_datetime("2020-01-25"),
+                            pd.to_datetime("2020-01-26"),
+                            pd.to_datetime("2020-01-27"),
+                            pd.to_datetime("2020-01-28"),
+                        ],
+                        freq=datetime.timedelta(days=1),
+                    ),
+                ),
+            ),
+            pd.Series(
+                data=[
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                    np.nan,
+                    5.0,
+                    5.0,
+                    5.0,
+                    5.0,
+                    5.0,
+                    5.0,
+                    4.0,
+                    4.0,
+                    4.0,
+                    4.0,
+                    4.0,
+                    4.0,
+                    4.0,
+                    4.0,
+                    4.0,
+                ],
+                index=pd.DatetimeIndex(
+                    data=[
+                        pd.to_datetime("2020-01-01"),
+                        pd.to_datetime("2020-01-02"),
+                        pd.to_datetime("2020-01-03"),
+                        pd.to_datetime("2020-01-04"),
+                        pd.to_datetime("2020-01-05"),
+                        pd.to_datetime("2020-01-06"),
+                        pd.to_datetime("2020-01-07"),
+                        pd.to_datetime("2020-01-08"),
+                        pd.to_datetime("2020-01-09"),
+                        pd.to_datetime("2020-01-10"),
+                        pd.to_datetime("2020-01-11"),
+                        pd.to_datetime("2020-01-12"),
+                        pd.to_datetime("2020-01-13"),
+                        pd.to_datetime("2020-01-14"),
+                        pd.to_datetime("2020-01-15"),
+                        pd.to_datetime("2020-01-16"),
+                        pd.to_datetime("2020-01-17"),
+                        pd.to_datetime("2020-01-18"),
+                        pd.to_datetime("2020-01-19"),
+                        pd.to_datetime("2020-01-20"),
+                        pd.to_datetime("2020-01-21"),
+                        pd.to_datetime("2020-01-22"),
+                        pd.to_datetime("2020-01-23"),
+                        pd.to_datetime("2020-01-24"),
+                        pd.to_datetime("2020-01-25"),
+                        pd.to_datetime("2020-01-26"),
+                        pd.to_datetime("2020-01-27"),
+                        pd.to_datetime("2020-01-28"),
+                    ],
+                    freq=datetime.timedelta(days=1),
+                ),
+            ),
+        )
