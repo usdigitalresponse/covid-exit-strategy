d0b1e386ac8a2b718cfed6632c4ac5eae836a761
BriceWolfgang <BriceWolfgang@gmail.com>
2020-06-07 18:40:08 -0400

    cdc data

--- covid/extract.py
+++ covid/extract.py
@@ -2,6 +2,7 @@ import json
 
 import pandas as pd
 import requests
+import covid.extract_config.analysis_usgovcloudapi as ugc
 
 
 DATE_SOURCE_FIELD = "date"
@@ -44,3 +45,59 @@ def get_state_abbreviations_to_names():
         abbreviations = json.load(state_abbreviations_file)
 
     return abbreviations
+
+
+def power_bi_extractor(response):
+    data = json.loads(response.text)
+    value_list = data['results'][0]['result']['data']['dsr']['DS'][0]['PH'][1]['DM1']
+    for vl in value_list:
+        yield vl['C']
+
+
+# CDC data source: https://www.cdc.gov/nhsn/covid19/report-patient-impact.html
+
+def extract_cdc_inpatient_beds():
+    # State Representative Estimates for Percentage of Inpatient Beds Occupied (All Patients)
+    response = requests.post(
+        ugc.URL,
+        headers={**ugc.BASE_HEADERS, **ugc.INPATIENT_BED_HEADERS},
+        data=open("./covid/extract_config/inpatient_bed_query.json"))
+
+    df = pd.DataFrame(
+        power_bi_extractor(response),
+        columns=["State", "inpatient_bed_percent_occupied", "inpatient_beds_occupied"])
+
+    df = df.set_index('State')
+
+    return df
+
+
+def extract_cdc_icu_beds():
+    # State Representative Estimates for Percentage of ICU Beds Occupied (All Patients)
+    response = requests.post(
+        ugc.URL,
+        headers={**ugc.BASE_HEADERS, **ugc.ICU_BED_HEADERS},
+        data=open("./covid/extract_config/icu_bed_query.json"))
+
+    df = pd.DataFrame(
+        power_bi_extractor(response),
+        columns=["State", "icu_percent_occupied", "icu_beds_occupied"])
+
+    df = df.set_index('State')
+
+    return df
+
+
+def extract_cdc_facilities_reporting():
+    response = requests.post(
+        ugc.URL,
+        headers={**ugc.BASE_HEADERS, **ugc.FACILITIES_REPORTING_HEADERS},
+        data=open("./covid/extract_config/facilities_reporting_query.json"))
+    
+    df = pd.DataFrame(
+        power_bi_extractor(response),
+        columns=["State", "facilities_percent_reporting", "facilities_reporting"])
+
+    df = df.set_index('State')
+
+    return df
