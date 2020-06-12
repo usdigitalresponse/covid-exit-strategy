c0e63e41896b86512a0038e0e51fbbf04840cec2
Lucas Brown <lmbrown@myst.ai>
2020-06-11 17:31:39 -0700

    adding last updated field everywhere and running black

--- covid/extract.py
+++ covid/extract.py
@@ -2,6 +2,7 @@ import json
 
 import pandas as pd
 import requests
+
 import covid.extract_config.cdc_govcloud as cgc
 
 
@@ -50,9 +51,9 @@ def get_state_abbreviations_to_names():
 def power_bi_extractor(response):
     data = json.loads(response.text)
     timestamp = extract_cdc_data_date()
-    value_list = data['results'][0]['result']['data']['dsr']['DS'][0]['PH'][1]['DM1']
+    value_list = data["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][1]["DM1"]
     for vl in value_list:
-        yield vl['C'] + [timestamp]
+        yield vl["C"] + [timestamp]
 
 
 # CDC data source: https://www.cdc.gov/nhsn/covid19/report-patient-impact.html
@@ -63,10 +64,11 @@ def extract_cdc_data_date():
     response = requests.post(
         cgc.URL,
         headers={**cgc.BASE_HEADERS, **cgc.DATA_DATE_HEADERS},
-        data=open("./covid/extract_config/data_date.json"))
+        data=open("./covid/extract_config/data_date.json"),
+    )
 
     data = json.loads(response.text)
-    return data['results'][0]['result']['data']['dsr']['DS'][0]['PH'][0]['DM0'][0]['M0']
+    return data["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][0]["DM0"][0]["M0"]
 
 
 def extract_cdc_inpatient_beds():
@@ -74,7 +76,8 @@ def extract_cdc_inpatient_beds():
     response = requests.post(
         cgc.URL,
         headers={**cgc.BASE_HEADERS, **cgc.INPATIENT_BED_HEADERS},
-        data=open("./covid/extract_config/inpatient_bed_query.json"))
+        data=open("./covid/extract_config/inpatient_bed_query.json"),
+    )
 
     df = pd.DataFrame(
         power_bi_extractor(response),
@@ -82,9 +85,11 @@ def extract_cdc_inpatient_beds():
             "State",
             "inpatient_bed_percent_occupied",
             "inpatient_beds_occupied",
-            "timestamp"])
+            "timestamp",
+        ],
+    )
 
-    df = df.set_index('State')
+    df = df.set_index("State")
 
     return df
 
@@ -94,17 +99,15 @@ def extract_cdc_icu_beds():
     response = requests.post(
         cgc.URL,
         headers={**cgc.BASE_HEADERS, **cgc.ICU_BED_HEADERS},
-        data=open("./covid/extract_config/icu_bed_query.json"))
+        data=open("./covid/extract_config/icu_bed_query.json"),
+    )
 
     df = pd.DataFrame(
         power_bi_extractor(response),
-        columns=[
-            "State",
-            "icu_percent_occupied",
-            "icu_beds_occupied",
-            "timestamp"])
+        columns=["State", "icu_percent_occupied", "icu_beds_occupied", "timestamp"],
+    )
 
-    df = df.set_index('State')
+    df = df.set_index("State")
 
     return df
 
@@ -113,7 +116,8 @@ def extract_cdc_facilities_reporting():
     response = requests.post(
         cgc.URL,
         headers={**cgc.BASE_HEADERS, **cgc.FACILITIES_REPORTING_HEADERS},
-        data=open("./covid/extract_config/facilities_reporting_query.json"))
+        data=open("./covid/extract_config/facilities_reporting_query.json"),
+    )
 
     df = pd.DataFrame(
         power_bi_extractor(response),
@@ -121,8 +125,10 @@ def extract_cdc_facilities_reporting():
             "State",
             "facilities_percent_reporting",
             "facilities_reporting",
-            "timestamp"])
+            "timestamp",
+        ],
+    )
 
-    df = df.set_index('State')
+    df = df.set_index("State")
 
     return df
